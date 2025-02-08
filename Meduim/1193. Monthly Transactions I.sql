7f618231d66c8637db6d6ff66d947039393aaeb9
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-02-08 03:30:25 +0200

    1193. Monthly Transactions I

---
+++ Meduim/1193. Monthly Transactions I.sql
@@ -0,0 +1,3 @@
+select DATE_FORMAT(trans_date, '%Y-%m') as month , country , count(*) as trans_count , sum(state="approved")as approved_count , sum(amount) as trans_total_amount , sum(if(state="approved",amount,0)) as approved_total_amount 
+from transactions
+group by month,country
