05f526dde2de2732c64d3ed13a946f62d06f1e63
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-02-14 22:18:06 +0200

    Window Functions (LAG()/LEAD()) --Mesh Jussyyy

--- Meduim/180. Consecutive Numbers.sql
+++ Meduim/180. Consecutive Numbers.sql
@@ -3,4 +3,14 @@ select distinct l1.num as ConsecutiveNums
 from logs l1
 join logs l2
 join logs l3
-on l1.num=l2.num and l2.num=l3.num and l3.id=l2.id-1 and l2.id=l1.id-1
+on l1.num=l2.num and l2.num=l3.num and l3.id=l2.id-1 and l2.id=l1.id-1;
+
+
+# window functions LAG/LEAD
+select distinct num as ConsecutiveNums from (
+    select num, 
+           LAG(num) OVER (ORDER BY id) AS prev_num,
+           LEAD(num) OVER (ORDER BY id) AS next_num
+    FROM logs
+) t
+where num = prev_num and num = next_num;
