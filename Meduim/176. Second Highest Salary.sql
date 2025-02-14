541ecdbff3daee0f5931e75b3b63f5c1af4af138
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-02-15 01:02:07 +0200

    Using Limit offset

--- Meduim/176. Second Highest Salary.sql
+++ Meduim/176. Second Highest Salary.sql
@@ -3,3 +3,9 @@
 from employee
  where salary<(select max(salary)from employee);
 
+## limit
+select ifnull ((
+select distinct salary 
+from employee
+order by salary desc
+limit 1 offset 1), null) as SecondHighestSalary
